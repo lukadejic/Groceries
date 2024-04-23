b782f65bdda29e1e82dd0e0d58a89c4441a3506b
Luka <lukadejic111@gmail.com>
2024-04-23 10:31:44 +0200

    Initial Commit

---
+++ Groceries/GroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesApp.swift
+//  Groceries
+//
+//  Created by MacBook on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
